97a2d55f99336dd2f84112e589201ecbecc9e67d
hr0123 <sallygofl@gmail.com>
2022-03-14 18:26:09 +0900

    첫 잔디

---
+++ Algorithm/example.js
@@ -0,0 +1,11 @@
+let move = 0; //이동 횟수
+let current = 1; //현재 위치
+
+move = move+1
+current = current+2 //두 층 올라감
+//49번 반복
+
+if(current!==100) {
+  current = current+1
+  move = move+1
+}
---
+++ class/pages/01-01-qqq/aaa/bbb/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+
+   //여기는 자바스크립트 쓰는 곳
+
+  return (
+
+  )
+}
