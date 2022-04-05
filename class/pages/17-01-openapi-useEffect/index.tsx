47d0df221d3b2ec0658be5b0955cf1c0ea4ca98a
hr0123 <sallygofl@gmail.com>
2022-04-05 13:24:00 +0900

    220405

---
+++ class/pages/17-01-openapi-useEffect/index.tsx
@@ -0,0 +1,26 @@
+import { useQuery } from "@apollo/client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function OpenapiWithUseEffectPage() {
+  const [dogUrl, setDogUrl] = useState("");
+
+  // graphql-api
+  // const {} = useQuery()
+
+  // rest-api (axios)(or 전문작업 시 react의 useQuery사용)
+  useEffect(() => {
+    const aaa = async () => {
+      const result = await axios.get("https://dog.ceo/api/breeds/image/random");
+      setDogUrl(result.data.message);
+    };
+    aaa();
+  }, []);
+
+  return (
+    <>
+      <div>오픈API 연습!!</div>
+      <img src={dogUrl} />
+    </>
+  );
+}
